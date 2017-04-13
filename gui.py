77cbb540b1db18197d90d034a97f382a0d16ce64
Wayde <wad67@live.com.au>
2017-04-13 11:15:23 +1000

    Added tabulated frames, got name working
    Also fixed a crash

--- gui.py
+++ gui.py
@@ -2,30 +2,33 @@
 from appJar import gui
 
 # top slice - CREATE the GUI
-
-
-def press(btnName):
-    if btnName == "quit":
-        app.stop()
-        return
-    if btnName == "starttimer":
-        starttimer()
-        return
-    if btnName == "stoptimer":
-        stoptimer()
-        return
-    if btnName == "submitname":
-		name = userEnt
-		print(name)
-		return
-
 app = gui("Timecalc - GUI V01")
+name = "NULL"
+def submitname(var):
+	print "Submitted name:"
+	name = app.getEntry("customername")
+	print (name)
+
+
+
 ### fillings go here ###
 app.addLabel("title", "Time Calculator")
 app.setLabelBg("title", "red")
-app.addLabel("custname", "Customer Name:", 0, 0)
-app.addEntry("userEnt", 0, 1)
-app.addButton("Submit Name", submitname)
+app.startTabbedFrame("TabbedFrame")
+app.startTab("Customer")
+app.addLabel("label_custname", "Name:")
+app.addEntry("customername")
+app.addButton("Submit", submitname)
+app.stopTab()
+
+app.startTab("Time Calculation")
+app.addLabel("label_starttime_prefix", "Start time:")
+app.addLabel("label_starttime", "<get current start time here>")
+app.addLabel("elapsed_time_prefix", "Elapsed time:")
+app.addLabel("label_currentime", "<store time elapsed here>")
+app.stopTab()
+
+app.stopTabbedFrame()
 
 
 # bottom slice - START the GUI
