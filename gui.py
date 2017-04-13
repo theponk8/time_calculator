d2a0382c0390a167b86e6d3e65c87f5ccf779144
Wayde <wad67@live.com.au>
2017-04-13 11:19:52 +1000

    Added comments

--- gui.py
+++ gui.py
@@ -1,9 +1,15 @@
 # import the library
 from appJar import gui
+### http://appjar.info/pythonWidgets/
+### http://appjar.info/pythonWidgetGrouping/
 
 # top slice - CREATE the GUI
 app = gui("Timecalc - GUI V01")
+
+#set our customer name to null if we just started
 name = "NULL"
+#this is what happens when we click the submit button for name
+#it must accept an input, even if you don't need one
 def submitname(var):
 	print "Submitted name:"
 	name = app.getEntry("customername")
@@ -12,6 +18,10 @@ def submitname(var):
 
 
 ### fillings go here ###
+#addLabel ([ident],[text])
+#setLabelBg ([ident],[colour])
+#addButton ([ident],[function to to call])
+
 app.addLabel("title", "Time Calculator")
 app.setLabelBg("title", "red")
 app.startTabbedFrame("TabbedFrame")
