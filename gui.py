d5f9418bcc62b62d5706e7be4fb08319d7097650
Wayde <wad67@live.com.au>
2017-04-14 16:12:11 +1000

    The main loop now does awful things

--- gui.py
+++ gui.py
@@ -4,34 +4,46 @@ from appJar import gui
 import time
 ### http://appjar.info/pythonWidgets/
 ### http://appjar.info/pythonWidgetGrouping/
+def initialise():
+	print "initialising variables"
+	global name, seconds, minutes, hours, timerstarted
+	name = "NULL"
+	seconds = 0
+	minutes = 0
+	hours = 0
+	timerstarted = 0
+	
+init = 0
+
+if init == 0:
+    initialise()
+    init = 1
+
 
 
 # top slice - CREATE the GUI
 app = gui("Timecalc - GUI V01")
 
 #our timer has not started yet
-timerstarted = 0
-
 #we have not counted any seconds, minutes or hours
-seconds = 0
-minutes = 0
-hours = 0
-
 #set our customer name to null if we just started
-name = "NULL"
+#tie all of these into a def, so it does not get called and reset our variables every loop
 
+	
 #A loop that is supposed to count our seconds
 def mainloop():
 	print "Executing main loop"
 	#this if statement refuses to trigger
 	#don't know how to make an interruptable loop with a gui
 	#check if our timer is on or off, if it's on then do the stuff
-	if timerstarted == '1':
+	print timerstarted
+	if timerstarted == 1:
 		print "timestuff happened"
 		seconds = seconds + 1
 		time.sleep(1)
 		#needs an interrupt of some description, realistically the if statement should work, but it does not
 		mainloop()
+	mainloop()
 	
 		
 			
@@ -46,9 +58,11 @@ def submitname(var):
 	
 #this triggers when you click the start button
 def starttimer(var):
-	print "Started timer"
+	print "Start Clicked"
 	#this is the control variable for the timer, setting it to one should turn it on
+	#for some reason, it keeps being set to 0
 	timerstarted = 1
+	print timerstarted
 	#update the start time in the gui with the current time as we just started
 	starttime = time.ctime()
 	app.setLabel("label_starttime", starttime)
@@ -61,7 +75,7 @@ def starttimer(var):
 
 #this is what happens when we click the stop button
 def stoptimer(var):
-	print "Stopped timer"
+	print "Stop Clicked"
 	#our timer control variable is set to zero, off
 	timerstarted = 0
 	
